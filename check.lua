6bc11eda1d6e10e69ba350aabb810c103b00a392
Jaras <daermlo346@gmail.com>
2025-12-12 00:42:19 +0700

    Change check interval to 3 seconds and countdown to 180

---
+++ check.lua
@@ -0,0 +1,39 @@
+repeat
+    task.wait()
+until game:IsLoaded()
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local Main = LocalPlayer.PlayerGui:FindFirstChild("Main")
+local Gold = Main and Main.Screen.Hud.Gold
+
+local oldMoney = Gold.Text
+
+local CHECK_INTERVAL = 3
+local COUNTDOWN_TIME = 180
+
+getgenv().LOADED = true
+warn("[", os.date("%H:%M:%S"), "] LOADED = true")
+
+while task.wait(CHECK_INTERVAL) do
+    print("[System] Working... Checking gold changes...")
+
+    pcall(function()
+        local newMoney = Gold.Text
+
+        if newMoney == oldMoney then
+            warn("No change in coins detected! Starting countdown to kick...")
+
+            for i = COUNTDOWN_TIME, 1, -1 do
+                warn("Kicking in " .. i .. " seconds... (System Running)")
+                task.wait(1)
+            end
+
+            LocalPlayer:Kick("\nRejoining due to no coin change...")
+        else
+            print("[System] Gold changed! System running normally.")
+            oldMoney = newMoney
+        end
+    end)
+end
