31766c058f3cb8b6b7a14b56788d7da37a04877b
Jaras <daermlo346@gmail.com>
2025-12-12 16:14:02 +0700

    Update asdhgawdya.lua

--- asdhgawdya.lua
+++ asdhgawdya.lua
@@ -1,5 +1,4 @@
 repeat task.wait(1) until game:IsLoaded()
-repeat task.wait() until _G.Horst_SetDescription and _G.DataConfigs
 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
